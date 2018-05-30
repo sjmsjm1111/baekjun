1be8fc8768fb827b3d00c1a6b3720857168fa86c
sjmsjm1111 <sjmsjm11111111111@gmail.com>
2018-05-31 00:25:34 +0900

    for baekjun code.
    I insert the 'container1' array in main function. But main function can't contain about 1 million length array. So, I declare 2 millon array at outside of main function. there are 2million of 1 dimesion array declaration in the local memory(?).

--- merge_sort/top_down_MS.c
+++ merge_sort/top_down_MS.c
@@ -1,1 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int dummy[1000000];
+int container1[1000000];
+void MS_top_down(int *arr,int start,int end);
+void merge(int *arr, int start, int end);
+
+int main(int argc, char *argv[]) {
+	int len,i;
+
+	scanf("%d",&len);
+	
+	for(i=0;i<len;i++){
+		scanf("%d",&(container1[i]));
+	}
+	MS_top_down(container1,0,len-1);
+	for(i=0;i<len;i++){
+		printf("%d ",container1[i]);
+	}
+	printf("\n");
+	return 0;
+}
+
+void MS_top_down(int *arr,int start,int end){
+	if(start<end){
+		int half=(start+end)/2 + 1;
+		MS_top_down(arr,start,half-1);
+		MS_top_down(arr,half,end);
+		merge(arr,start, end);
+	}
+}
+void merge(int *arr, int start, int end){
+	int half=(start+end)/2 + 1;//우선순위 되지?
+	int i=start,j=half;
+	int index=start;
+	while(i<half&&j<=end){
+		if(arr[i]>arr[j]){
+			dummy[index]=arr[j];
+			j++;
+		}
+		else{
+			dummy[index]=arr[i];
+			i++;
+		}
+		index++;
+	} 
+	while(i<half){
+		dummy[index]=arr[i];
+		i++;
+		index++;
+	}
+	while(j<=end){
+		dummy[index]=arr[j];
+		j++;
+		index++;
+	}
+	for(i=start;i<=end;i++){
+		arr[i]=dummy[i];
+	}
+	return;
+}
