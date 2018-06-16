2a2a01fe22ed4c1882b0425169389e147a18f64a
sjmsjm1111 <sjmsjm11111111111@gmail.com>
2018-06-16 22:11:26 +0900

    Update G-LCD_basic.cpp
    빨라진 이유는 밑에

--- GCD-LCD/G-LCD_basic.cpp
+++ GCD-LCD/G-LCD_basic.cpp
@@ -23,3 +23,38 @@ int main(int argc, char** argv) {
 	
 	return 0;
 }
+
+/*                         *****MORE FASTER VERSION*****
+#include <iostream>
+
+using namespace std;
+int A[1001],B[1001];
+
+int GCD(int a, int b){
+	if(b>a) return GCD(b,a);
+	if(!b) return a;
+	return GCD(b,a%b);
+}
+int LCD(int a, int b){
+	int gcd=GCD(a,b);
+	return gcd*(a/gcd)*(b/gcd);
+}
+
+int main(int argc, char** argv) {
+	int test_case;
+    int temp;
+	cin>>test_case;
+	temp=test_case;
+	while(test_case--){
+		cin>>A[test_case]>>B[test_case];
+	}
+    while(temp--){
+        cout<<LCD(A[temp],B[temp])<<"\n";
+    }
+	
+	return 0;
+}
+
+1996 KB	0 MS	C++14 / 수정	493 B
+because of seperate input with output(calculator). compiler can optimize the codes. 
+*/
